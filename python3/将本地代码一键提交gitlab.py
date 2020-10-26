77ea8703dd3bc258cf4b1fb5148256224584904b
小强饲养员 <32380837+xiaoliang002@users.noreply.github.com>
2020-10-26 18:05:59 +0800

    Create 将本地代码一键提交gitlab.py

---
+++ python3/将本地代码一键提交gitlab.py
@@ -0,0 +1,13 @@
+import git
+from  datetime import *
+import os
+
+try:
+    repo = git.Repo('/Users/alias/opstool')
+    remote = repo.remote() #创建对象
+    print(remote.pull('master')) #下拉代码
+    print(repo.git.add('*')) #添加本地文件
+    print(repo.git.commit(m=datetime.now())) #提交代码
+    print(remote.push('master')) #推送代码
+except Exception as e:
+    print(e)
